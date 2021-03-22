ddf3e39e1129c6bacbf71b2f08690b40522805c4
jamezjaz <jamezjaz@gmail.com>
2021-03-22 07:56:09 -0500

    docs: Implement login features
    docs: Adds login method
    docs: Adds test method for authentication and authorization
    docs: Defines authenticate as a private method
    docs: Skips authenticate request for login and register since we can't authorize unregistered users or users who wants login

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,17 @@
 class UsersController < ApplicationController
-   # POST /register
-   def register
+  skip_before_action :authenticate_request, only: %i[login register]
+
+  def login
+    authenticate params[:email], params[:password]
+  end
+  def test
+    render json: {
+        message: 'You have passed authentication and authorization test'
+      }
+  end
+
+  # POST /register
+  def register
     @user = User.create(user_params)
    if @user.save
     response = { message: 'User created successfully'}
@@ -11,6 +22,19 @@ class UsersController < ApplicationController
   end
 
   private
+
+  def authenticate(email, password)
+    command = AuthenticateUser.call(email, password)
+
+    if command.success?
+      render json: {
+        access_token: command.result,
+        message: 'Login Successful'
+      }
+    else
+      render json: { error: command.errors }, status: :unauthorized
+    end
+   end
 
   def user_params
     params.permit(
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  #add the register route
   post 'auth/register', to: 'users#register'
+  post 'auth/login', to: 'users#login'
+  get 'test', to: 'users#test'
 end
