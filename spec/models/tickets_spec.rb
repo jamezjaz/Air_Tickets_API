72821208445d88cb238fca97391fc72341886a02
jamezjaz <jamezjaz@gmail.com>
2021-03-28 07:33:33 -0500

    docs: Updates validations tests on Ticket model

--- spec/models/tickets_spec.rb
+++ spec/models/tickets_spec.rb
@@ -5,4 +5,13 @@ RSpec.describe Ticket, type: :model do
     it { should belong_to(:user) }
     it { should belong_to(:airline) }
   end
+
+  context 'Ticket validations' do
+    it { is_expected.to have_db_column(:airline_name) }
+    it { is_expected.to have_db_column(:username) }
+    it { is_expected.to have_db_column(:city) }
+    it { is_expected.to have_db_column(:date) }
+    it { is_expected.to have_db_column(:user_id) }
+    it { is_expected.to have_db_column(:airline_id) }
+  end
 end
