436627c7a24e0fbada704342804dd2b92260ad98
Jamezjaz <jamezjaz@gmail.com>
2024-07-19 20:41:40 +0100

    Updated Ruby version.
    Updated the database.yml config file.
    Seeded some data in the db/seed.rb file.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,50 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+
+# db/seeds.rb
+Airline.create([
+  {
+    name: "Air Peace",
+    price: 350.00,
+    location: "Nigeria",
+    description: "Air Peace is one of the largest airlines in the world.",
+    image: File.open(Rails.root.join('public', 'images', 'air-peace.webp'))
+  },
+  {
+    name: "Delta Airlines",
+    price: 450.00,
+    location: "Atlanta",
+    description: "Delta Airlines is known for its excellent customer service.",
+    image: File.open(Rails.root.join('public', 'images', 'delta-airlines.jpg'))
+  },
+  {
+    name: "United Airlines",
+    price: 500.00,
+    location: "Chicago",
+    description: "United Airlines offers flights to numerous destinations worldwide.",
+    image: File.open(Rails.root.join('public', 'images', 'united-airlines.webp'))
+  },
+  {
+    name: "British Airways",
+    price: 600.00,
+    location: "London",
+    description: "British Airways is a premium international airline.",
+    image: File.open(Rails.root.join('public', 'images', 'british-airways.webp'))
+  },
+  {
+    name: "Emirates",
+    price: 700.00,
+    location: "Dubai",
+    description: "Emirates offers luxurious flights and exceptional service.",
+    image: File.open(Rails.root.join('public', 'images', 'emirates.jpeg'))
+  },
+  {
+    name: "Qatar Airways",
+    price: 750.00,
+    location: "Doha",
+    description: "Qatar Airways is known for its outstanding in-flight experience.",
+    image: File.open(Rails.root.join('public', 'images', 'qatar-airways.webp'))
+  }
+])
